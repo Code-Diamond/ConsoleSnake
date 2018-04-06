4181dd6fc00a7f298b06041f8cc2c46e6a10f526
JaminROCK <KingJamin@hotmail.com>
2018-04-06 14:50:04 -0500

    Update ConsoleSnake.cpp
    modularized

--- ConsoleSnake.cpp
+++ ConsoleSnake.cpp
@@ -2,19 +2,49 @@
 
 using namespace std;
 
+char** CreateMap();
+void UpdateMap(char**);
+void DisplayView(char**);
+void AskToContinue();
+int x = 3;
+int y = 3;
+
 int main()
 {
+	//Create map
+	char** map = CreateMap();
+	
+	bool gameLoop = true;
+	//Game loop
+	while (gameLoop)
+	{
+		UpdateMap(map);
+		DisplayView(map);
+		//If snake accidently hits himself
+		gameLoop = false;
+	}
+
+
+	AskToContinue();
+
+	return 0;
+}
+//Initial creation of the gmae map array
+char** CreateMap()
+{
 	char** map = new char*[20];
-	int x, y;
-	x = 3;
-	y = 3;
+
 
 	for (int i = 0; i < 20; i++)
 	{
 		map[i] = new char[20];
 	}
 
-
+	return map;
+}
+//Logic for updating the game map
+void UpdateMap(char** map)
+{
 	for (int i = 0; i < 20; i++)
 	{
 		for (int j = 0; j < 20; j++)
@@ -56,22 +86,19 @@ int main()
 							{
 								map[i][j] = ' ';
 							}
-							
 						}
 					}
 				}
 			}
-
-			
-
-			
-
-	
-			
 		}
 	}
-
-
+}
+//Prints the map
+void DisplayView(char** map)
+{
+	//Clear screen
+	system("CLS");
+	//Print map
 	for (int i = 0; i < 20; i++)
 	{
 		for (int j = 0; j < 20; j++)
@@ -80,10 +107,11 @@ int main()
 		}
 		cout << endl;
 	}
-
+}
+//Ask user to press any key and enter to continue
+void AskToContinue()
+{
 	cout << "Press any key and then enter to exit." << endl;
 	char continueKey;
 	cin >> continueKey;
-
-	return 0;
 }
